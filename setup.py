54564896e556a14a6b1e325c193c1a5539be1e79
marikokamiya <155508932+MKmotion@users.noreply.github.com>
2024-02-26 22:41:56 +0900

    PACKAGE?

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+#
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='MultiSourceToMarkdown',
+    version='1.0.0',
+    author='kenny_anoano',
+    author_email='your.email@example.com',
+    description='A tool to convert various source formats into Markdown',
+    packages=find_packages(),
+    install_requires=requirements,
+)
